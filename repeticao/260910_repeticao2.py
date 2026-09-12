bd7657efb7fe9ae8a24d3d9b8a1a45c65528fba7
JardelRC <jardelcardoso.mao@gmail.com>
2026-09-11 20:34:16 -0400

    Avaliação 1

--- repeticao/260910_repeticao2.py
+++ repeticao/260910_repeticao2.py
@@ -1,16 +1,18 @@
+####################################################
 # Atividade 3: Média de 4 notas
 soma = 0
 
-for numero in range(1,5):
-    nota = float(input("Digite uma nota: "))
+for numero in range(1, 5):
+    nota = float(input("Digite uma nota (0,0-10,0): "))
     soma = soma + nota
 
 media = soma / 4
 print("A média é:", media)
 
+####################################################
 # Atividade 4: Estoque de produtos
-for numero in range(1,6):
+for numero in range(1, 6):
     produto = str(input("Digite o nome de um produto: "))
-    print(f"{produto} cadastrado  no estoque.")
+    print(f"{produto} cadastrado no estoque.")
 
 print("Produtos cadastrados.")
